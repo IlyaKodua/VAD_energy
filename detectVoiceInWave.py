c44be597faed471d2e2d9333838a8d9732ec9a10
Ilya Kodua <kodua.stc@gmail.com>
2022-06-15 20:02:09 +0300

    [+] add pics

--- detectVoiceInWave.py
+++ detectVoiceInWave.py
@@ -5,13 +5,6 @@ import os
 
 if __name__ == "__main__":
 
-# 4 noise 0.4489931273660462
-# 6 speech 0.906390297803528
-# 7 music 0.7905428903534732 
-# 8 speech 0.871315123426183
-# 11 speech 0.7950834410026628
-# 13 speech 0.6127030494891371
-# 14 noise 0.2962122201590884
     # path_to_save
     dir = "pics"
     os.mkdir("pics")
